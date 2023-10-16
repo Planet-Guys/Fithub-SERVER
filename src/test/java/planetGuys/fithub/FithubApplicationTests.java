504a80ae44a50ec20cb9c1d9c475c5ce43cd5917
CYY1007 <younguk86@gmail.com>
2023-10-16 19:07:50 +0900

    :tada: 프로젝트 세팅

---
+++ src/test/java/planetGuys/fithub/FithubApplicationTests.java
@@ -0,0 +1,13 @@
+package planetGuys.fithub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FithubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
